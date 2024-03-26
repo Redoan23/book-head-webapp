9f08df7436663c2d7d1f44aef4c8eac9eb30da7b
Redoan23 <redoan1080@gmail.com>
2024-03-27 03:52:11 +0600

    fixed error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -11,14 +11,7 @@ export default function Error() {
     return (
         <div className="max-w-screen-2xl mx-auto flex flex-col justify-center items-center gap-10 absolute top-52 left-[50%]">
             <i>{`fatal error ${error.status}`}</i>
-
-            {
-                error.status === 404 && <div>
-                    <p>page not found</p>
-                    <button onClick={handleNavigate} >Go Back</button>
-                </div>
-            }
-
+            <p>page not found</p>
             <button className="btn text-center" onClick={handleNavigate}>Go back?</button>
         </div>
     )
